a91788ea5a38add4a1b5038ae3b339fab6885b16
devdrsh <devdrsh33@gmail.com>
2025-11-04 22:31:47 +0530

    Refactor imports and update page icon
    Removed duplicate imports and updated page icon.

--- workreport_web.py
+++ workreport_web.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="MB Report",
-    page_icon="Mblogo.png",
+    page_icon="MB.png",
     layout="wide"
 )
 
@@ -182,6 +182,7 @@ if "report_text" in st.session_state:
         file_name=f"WorkReport_{datetime.date.today()}.txt",
         mime="text/plain"
     )
+
 
 
 
